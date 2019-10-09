fb714eefe4726591144b07e9568fd3a0f803c5d4
Angela Li <=>
2019-10-09 14:04:06 -0500

    Add sf version of NYC dataset

--- R/nyc.R
+++ R/nyc.R
@@ -1,6 +1,6 @@
 #' Rental housing and demographics in NYC (2000s).
 #'
-#' Demographic and housing data for New York City’s 55 sub-boroughs (2000s).
+#' Demographic and housing data for New York City’s 55 sub-boroughs (2000s). Dataframe, no spatial components.
 #'
 #' @format A data frame with 55 rows and 34 variables:
 #' \describe{
@@ -17,3 +17,25 @@
 #' }
 #' @source \url{https://geodacenter.github.io/data-and-lab/nyc/}
 "nyc"
+
+#' Rental housing and demographics in NYC (2000s).
+#'
+#' Demographic and housing data for New York City’s 55 sub-boroughs (2000s).
+#' Sf object, units in ft. EPSG 2263: NAD83 / New York Long Island (ftUS).
+#'
+#' @format An sf data frame with 55 rows, 34 variables, and a geometry column:
+#' \describe{
+#'   \item{forhis06-09}{percentage of hispanic population, not born in US}
+#'   \item{forwh06-09}{percentage of white population, not born in US}
+#'   \item{hhsiz1990}{average number of people per household}
+#'   \item{hhsiz00}{average number of people per household}
+#'   \item{hhsiz02-05-08}{average number of people per household}
+#'   \item{kids2000, kids2005-2009}{percentage households w kids under 18}
+#'   \item{rent2002,2005,2008}{median monthly contract rent}
+#'   \item{rentpct02,05,08}{percentage of housing stock that is market rate rental units}
+#'   \item{pubast90,00}{percentage of households receiving public assistance}
+#'   \item{yrhom02,05,08}{average number of years living in current residence}
+#'   \item{geometry}{MULTIPOLYGON}
+#' }
+#' @source \url{https://geodacenter.github.io/data-and-lab/nyc/}
+"nyc_sf"
---
+++ data-raw/clean-nyc-sf.R
@@ -0,0 +1,7 @@
+library(sf)
+library(usethis)
+
+nyc_sf <- st_read("data-raw/nyc.shp")
+nyc_sf <- lwgeom::st_make_valid(nyc_sf)
+
+usethis::use_data(nyc_sf, overwrite = TRUE)
