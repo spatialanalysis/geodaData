a2b645533052c67773a2e56e474096e300f7e8f5
Angela Li <=>
2019-10-31 10:28:45 -0500

    Fix CRS for ohio lung data (#5)

--- data-raw/clean-ohio-lung.R
+++ data-raw/clean-ohio-lung.R
@@ -5,4 +5,6 @@ ohio_lung <- st_read("data-raw/ohlung.geojson",
                      quiet = TRUE,
                      stringsAsFactors = FALSE)
 
+st_crs(ohio_lung) <- 32617
+
 usethis::use_data(ohio_lung, overwrite = TRUE)
